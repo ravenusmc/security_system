08c66945885c8900aae002affb3ac6659156d945
ravenusmc <mcuddy77@gmail.com>
2022-11-23 09:47:08 -0500

    started work on about page

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,4 +1,13 @@
 <?php require APPROOT . "/views/inc/header.php"; ?>
+<link rel='stylesheet' href="<?php echo URLROOT; ?>/css/generic.css">
+<link rel='stylesheet' href="<?php echo URLROOT; ?>/css/about.css">
+
+	<!-- Header area -->
+	<div class='about_header font'>
+		<h1 class='title font center'>About Me</h1>
+	</div>
+	<!-- End of Header Area -->
+	
 <h1>About</h1>
 
 <?php require APPROOT . "/views/inc/footer.php"; ?>
