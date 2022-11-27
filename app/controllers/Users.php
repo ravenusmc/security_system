e383135b8be2450382e564faecf15ba7e5c48da1
ravenusmc <mcuddy77@gmail.com>
2022-11-27 10:22:31 -0500

    started work on the users controller

---
+++ app/controllers/Users.php
@@ -0,0 +1,31 @@
+<?php 
+
+	class Users extends Controller {
+
+		public function __construct(){
+
+		}
+
+		public function register() {
+			// Check for post
+			if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+				// Process the form
+			} else {
+				// Init Data
+				$data = [
+					'name' => '',
+					'email' => '',
+					'password' => '',
+					'confirm_password' => '',
+					'name_err' => '',
+					'email_err' => '',
+					'password_err' => '',
+					'confirm_password_err' => '',
+				];
+
+				// Load View 
+				$this->view('users/register', $data);
+			}
+		}
+
+	}
