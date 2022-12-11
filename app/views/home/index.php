906795387426440cb94085db5f1c94c8c87676a6
ravenusmc <mcuddy77@gmail.com>
2022-12-11 10:59:52 -0500

    added buttons to systems page

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,9 +1,10 @@
 <?php require APPROOT . "/views/inc/header.php"; ?>
 <link rel='stylesheet' href="<?php echo URLROOT; ?>/css/generic.css">
 <link rel='stylesheet' href="<?php echo URLROOT; ?>/css/system.css">
+<link rel='stylesheet' href="<?php echo URLROOT; ?>/css/button.css">
 
 <section class='building-section'>
-	<div class='building-one center-text'>
+	<div id='building-one' class='building-one center-text'>
 		<h6>Building One</h6>
 	</div>
 	<div class='building-two center-text'>
@@ -11,6 +12,18 @@
 	</div>
 	<div class='building-three center-text'>
 		<h6>Building Three</h6>
+	</div>
+</section>
+
+<section class='button-section'>
+	<div>
+		<button class='button-look' onclick="turnOffAlarm()">Turn Off Alarm - 1</button>
+	</div>
+	<div>
+		<button class='button-look'>Turn Off Alarm - 2</button>
+	</div>
+	<div>
+		<button class='button-look'>Turn Off Alarm - 3</button>
 	</div>
 </section>
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,12 @@
+// console.log('hi')
+// let buildingOne = document.getElementById("building-one"); 
 
+// buildingOne.onclick = function() { //asign a function
+// //code
+// console.log('Work');
+// }
 
+function turnOffAlarm() {
+	console.log('dhadhhad');
+  // document.getElementById("demo").innerHTML = "Hello World";
+}
