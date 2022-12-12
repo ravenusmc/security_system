f5cf81fd07f68fa3d0de0321990e56949306a469
ravenusmc <mcuddy77@gmail.com>
2022-12-12 16:27:32 -0500

    system page is working at a basic level

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -7,23 +7,23 @@
 	<div id='building-one' class='building-one center-text'>
 		<h6>Building One</h6>
 	</div>
-	<div class='building-two center-text'>
+	<div id='building-two' class='building-two center-text'>
 		<h6>Building Two</h6>
 	</div>
-	<div class='building-three center-text'>
+	<div id='building-three' class='building-three center-text'>
 		<h6>Building Three</h6>
 	</div>
 </section>
 
 <section class='button-section'>
 	<div>
-		<button class='button-look' onclick="turnOffAlarm()">Turn Off Alarm - 1</button>
+		<button class='button-look' onclick="turnOffAlarm('one')">Turn Off Alarm - 1</button>
 	</div>
 	<div>
-		<button class='button-look'>Turn Off Alarm - 2</button>
+		<button class='button-look' onclick="turnOffAlarm('two')">Turn Off Alarm - 2</button>
 	</div>
 	<div>
-		<button class='button-look'>Turn Off Alarm - 3</button>
+		<button class='button-look' onclick="turnOffAlarm('three')">Turn Off Alarm - 3</button>
 	</div>
 </section>
 
--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,34 @@
 // console.log('Work');
 // }
 
-function turnOffAlarm() {
-	console.log('dhadhhad');
-  // document.getElementById("demo").innerHTML = "Hello World";
+
+
+setInterval(
+	function(){
+			let randomNumber = Math.floor((Math.random() * 3) + 1);
+			console.log(randomNumber)
+			if (randomNumber === 1){
+				let element = document.getElementById("building-one");
+				element.style.backgroundColor = "#00FF00";
+			}else if (randomNumber ===2){
+				let element = document.getElementById("building-two");
+				element.style.backgroundColor = "#00FF00";
+			} else {
+				let element = document.getElementById("building-three");
+				element.style.backgroundColor = "#00FF00";
+			}
+	},3000);
+
+
+function turnOffAlarm(value) {
+	if (value === "one") {
+		let element = document.getElementById("building-one");			
+		element.style.backgroundColor = "#FFFFFF";
+	}else if (value === "two") {
+		let element = document.getElementById("building-two");			
+		element.style.backgroundColor = "#FFFFFF";
+	}else {
+		let element = document.getElementById("building-three");			
+		element.style.backgroundColor = "#FFFFFF";
+	}
 }
