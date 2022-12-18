ae4435fa7cf1539f046ae466f39902f74ce83bbd
ravenusmc <mcuddy77@gmail.com>
2022-12-18 11:34:06 -0500

    added new function to hide alarm text

--- public/js/main.js
+++ public/js/main.js
@@ -12,32 +12,39 @@ function showAlarmText(element) {
 	}
 };
 
+// setInterval(
+// 	function(){
+// 			let randomNumber = Math.floor((Math.random() * 3) + 1);
+// 			if (randomNumber === 1){
+// 				let element = document.getElementById("building-one");
+// 				showAlarmText(element)
+// 				element.style.backgroundColor = "#FF0000";
+// 			}else if (randomNumber ===2){
+// 				let element = document.getElementById("building-two");
+// 				showAlarmText(element)
+// 				element.style.backgroundColor = "#FF0000";
+// 			} else {
+// 				let element = document.getElementById("building-three");
+// 				showAlarmText(element)
+// 				element.style.backgroundColor = "#FF0000";
+// 			}
+// 	}, 5000);
 
-setInterval(
-	function(){
-			let randomNumber = Math.floor((Math.random() * 3) + 1);
-			if (randomNumber === 1){
-				let element = document.getElementById("building-one");
-				showAlarmText(element)
-				element.style.backgroundColor = "#FF0000";
-			}else if (randomNumber ===2){
-				let element = document.getElementById("building-two");
-				showAlarmText(element)
-				element.style.backgroundColor = "#FF0000";
-			} else {
-				let element = document.getElementById("building-three");
-				showAlarmText(element)
-				element.style.backgroundColor = "#FF0000";
-			}
-	}, 5000);
-
-
-function hideAlarmText() {
-
+function hideAlarmText(element) {
+	let selectedBuilding = element.id
+	if (selectedBuilding === 'building-one') {
+		let textElement = document.getElementById("alarm-text-one");
+		textElement.style.display = 'none';
+	} else if (selectedBuilding === 'building-two') {
+		let textElement = document.getElementById("alarm-text-two");
+		textElement.style.display = 'none';
+	} else {
+		let textElement = document.getElementById("alarm-text-three");
+		textElement.style.display = 'none';
+	}
 }
 
 function checkAlarmOnOff(element) {
-	console.log(element.id)
 	let currentColor = parseInt(element.style.backgroundColor.slice(8, 12))
 	if (currentColor === 255) {
 		alert('No Alarm at building')
@@ -46,16 +53,18 @@ function checkAlarmOnOff(element) {
 	}
 };
 
-
 function turnOffAlarm(value) {
 	if (value === "one") {
 		let element = document.getElementById("building-one");
 		checkAlarmOnOff(element)
+		hideAlarmText(element)
 	} else if (value === "two") {
 		let element = document.getElementById("building-two");
 		checkAlarmOnOff(element)
+		hideAlarmText(element)
 	} else {
 		let element = document.getElementById("building-three");
 		checkAlarmOnOff(element)
+				hideAlarmText(element)
 	}
 }
