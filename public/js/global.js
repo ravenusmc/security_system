6b91434deb24cec94e6f8f7375b47eef32d2c126
ravenusmc <mcuddy77@gmail.com>
2022-12-31 14:13:02 -0500

    minor change

--- public/js/global.js
+++ public/js/global.js
@@ -1,4 +1,5 @@
 // This file will hold the global variables for the alarm system. 
+// I was taught to never use these but with JavaScript I find it easier...at times.
 
 // Declaring Global Variables
 let alarmSystemActive = true;
